efb26ef8667a3141fd1ee557ec7bf10db80e101d
Chris Ross <chrislross@outlook.com>
2020-07-02 11:43:32 -0400

    Clean up variable naming

--- BlackScholesCalc.py
+++ BlackScholesCalc.py
@@ -4,17 +4,16 @@ from scipy.stats import norm
 def get_call_price(spot, strike, rfr, vol, term):
     d1 = 1 / (vol * math.sqrt(term)) * (math.log(spot/strike) + (rfr + vol**2 / 2) * term)
     d2 = d1 - vol * math.sqrt(term)
-    price = norm.cdf(d1) * spot - norm.cdf(d2) * math.exp(-rfr * term) * strike;
-    return price
+    return norm.cdf(d1) * spot - norm.cdf(d2) * math.exp(-rfr * term) * strike;
 
 def main():
-    spotPrice = float(input("Spot price (ex: $30): ").replace("$",""))
-    exercisePrice = float(input("Exercise price (ex: $32.5): ").replace("$",""))
-    riskFreeRate = float(input("Risk free rate (ex: 5%): ").replace("%","")) / 100
+    spot_price = float(input("Spot price (ex: $30): ").replace("$",""))
+    exercise_price = float(input("Exercise price (ex: $32.5): ").replace("$",""))
+    risk_free_rate = float(input("Risk free rate (ex: 5%): ").replace("%","")) / 100
     volatility = float(input("Volatility (ex: 8.5%): ").replace("%","")) / 100
-    timeToMaturity = float(input("Time to expiration in years (ex: 1.5 years): ").replace("years","").replace("year",""))
-    callPrice = get_call_price(spotPrice, exercisePrice, riskFreeRate, volatility, timeToMaturity)
-    print("\nCall price: ${0}".format(callPrice))
+    time_to_maturity = float(input("Time to expiration in years (ex: 1.5 years): ").replace("years","").replace("year",""))
+    call_price = get_call_price(spot_price, exercise_price, risk_free_rate, volatility, time_to_maturity)
+    print("\nCall price: ${0}".format(call_price))
 
 if __name__ == "__main__":
     main()
